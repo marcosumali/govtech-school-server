a89b4d50c4dcd1c911a040dfaf714b05506b2e7d
Marco Sumali <marco.sumali90@gmail.com>
2021-10-13 00:18:50 +0700

    chore: add dotenv and body parser

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
+require('dotenv').config()
 const express = require('express')
 
 const app = express()
 const PORT = process.env.PORT || 8080
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
 app.listen(PORT, () => console.log(`App is listening at http://localhost:${PORT}`))
