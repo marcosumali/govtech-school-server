50e8872bba6246cfffb3a3b9924140693faf82d4
Marco Sumali <marco.sumali90@gmail.com>
2021-10-15 17:34:44 +0700

    chore: remove unused routes

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,11 @@
 const express = require('express');
 
 const apiRouter = require('./api');
-const teacherRouter = require('./teacher');
-const studentRouter = require('./student');
 
 const router = express.Router();
 
 router
   .use('/api', apiRouter)
-  .use('/teachers', teacherRouter)
-  .use('/students', studentRouter)
 
   
 module.exports = router;
