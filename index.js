a08da76fed1e3aa771410c7c3c257aaadd9460c4
Marco Sumali <marco.sumali90@gmail.com>
2021-10-16 13:11:02 +0700

    chore: add cors

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
 
 const rootRouter = require('./routes');
 
 const app = express();
 const PORT = process.env.PORT || 8080;
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
