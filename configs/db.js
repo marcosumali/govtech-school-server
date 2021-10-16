56b3f25c81e496305fc71b61214eb33dc6706965
Marco Sumali <marco.sumali90@gmail.com>
2021-10-16 20:22:30 +0700

    chore: add config to RDS SQL

--- configs/db.js
+++ configs/db.js
@@ -1,17 +1,19 @@
 const mysql = require('mysql2/promise');
 
 const {
-  HOST,
-  USER,
-  PASSWORD,
-  DATABASE,
+  DB_HOST,
+  DB_PORT,
+  DB_USER,
+  DB_PASSWORD,
+  DB_NAME,
 } = process.env
 
 const pool  = mysql.createPool({
-  host: HOST,
-  user: USER,
-  password: PASSWORD,
-  database: DATABASE
+  host: DB_HOST || 'localhost',
+  port: DB_PORT || 3306,
+  user: DB_USER,
+  password: DB_PASSWORD,
+  database: DB_NAME
 });
 
 
