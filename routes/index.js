4de3b766b47b7f0d5dfe3dbd90f933fddbd7f4d6
Marco Sumali <marco.sumali90@gmail.com>
2021-10-17 02:20:30 +0700

    fix: cicd app start spec

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const apiRouter = require('./api');
 
 const router = express.Router();
-const version = '0.0.1';
+const version = '0.0.2';
 
 router
   .get('/', (req, res) => res.send(`Version: v${version}`))
