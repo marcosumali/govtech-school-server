a44e8cd994b3d5d0fe26e68e3fbee4d5f65b4169
Marco Sumali <marco.sumali90@gmail.com>
2021-10-18 00:26:32 +0700

    chore: 404 error handler

--- index.js
+++ index.js
@@ -3,9 +3,10 @@ const express = require('express');
 const cors = require('cors');
 
 const rootRouter = require('./routes');
+const {generateError} = require('./helper/utils')
 
 const app = express();
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
@@ -14,6 +15,22 @@ app.use(express.urlencoded({ extended: true }));
 app.listen(PORT, () => console.log(`App is listening at http://localhost:${PORT}`));
 
 app.use('/', rootRouter);
+
+// Catch 404 and forward to error handler
+app.use((req, res, next) => {
+  const error = generateError(404, 'Path not found')
+  next(error);
+});
+
+// Error handler
+app.use((err, req, res, next) => {
+  if (err) {
+    const {code, error} = err
+    res.status(code).json({message: error.message})
+  }
+
+  res.status(500).json({message: 'Internal server error'})
+});
 
 
 module.exports = app;
